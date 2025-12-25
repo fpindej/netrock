8a39858d2fc54f81dfac2f48bf8143ad1626f6e4
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2025-12-26 00:00:48 +0100

    Perform cleanup

--- src/frontend/src/lib/components/ui/dropdown-menu/dropdown-menu-sub-trigger.svelte
+++ src/frontend/src/lib/components/ui/dropdown-menu/dropdown-menu-sub-trigger.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { DropdownMenu as DropdownMenuPrimitive } from 'bits-ui';
-	import ChevronRightIcon from '@lucide/svelte/icons/chevron-right';
+	import { ChevronRight } from 'lucide-svelte';
 	import { cn } from '$lib/utils.js';
 
 	let {
@@ -25,5 +25,5 @@
 	{...restProps}
 >
 	{@render children?.()}
-	<ChevronRightIcon class="ms-auto size-4" />
+	<ChevronRight class="ms-auto size-4" />
 </DropdownMenuPrimitive.SubTrigger>
